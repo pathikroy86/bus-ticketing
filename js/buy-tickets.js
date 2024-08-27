2921ac39440583e3c092c73d8d09e997e5696722
pathikroy86 <pathik.roy86@outlook.com>
2024-08-27 08:57:07 +0600

    modal

--- js/buy-tickets.js
+++ js/buy-tickets.js
@@ -12,4 +12,31 @@ document.getElementById("seat-select-container").addEventListener('click', funct
     newLiField("seat-number", seatValue);
     newLiField("economy-field", "Economy");
     newLiField("per-seat-price", 550);
+    const seatLeftField = document.getElementById("seats-left");
+    const seatLeftValue = seatLeftField.innerText;
+    const seatLeft = parseInt(seatLeftValue);
+    seatLeftField.innerText = seatLeft - 1;
+    const seatCountField = document.getElementById("seat-counter");
+    let seatCountValue = seatCountField.innerText;
+    let seatCount = parseInt(seatCountValue);
+    seatCountField.innerText = seatCount + 1;
+    const totalPriceField = document.getElementById("total-price");
+    const seatPurchased = seatCount + 1;
+    const totalPrice = seatPurchased * 550;
+    totalPriceField.innerText = totalPrice;
+    const grandTotalField = document.getElementById("grand-total");
+    grandTotalField.innerText = totalPrice;
+    removeDisable("name-input");
+    removeDisable("number-input");
+    removeDisable("email-input");
+    const number = document.getElementById("number-input").value;
+    const num = parseInt(number);
+    document.getElementById("number-input").addEventListener('keyup', function () {
+        removeDisable("submit-btn");
+    })
+    if (seatCount === 3) {
+        removeDisable("discount-coupon-field");
+
+    }
+
 });
--- js/utilities.js
+++ js/utilities.js
@@ -1,11 +1,10 @@
 
-function getValue(inputId) {
-    const inputField = document.getElementById(inputId);
-    const inputValue = inputField.innerText;
-    return inputValue;
-}
 function newLiField(inputId, value) {
     const newLi = document.createElement("li");
     newLi.innerText = value;
     document.getElementById(inputId).appendChild(newLi);
+}
+function removeDisable(inputId) {
+    const field = document.getElementById(inputId);
+    field.removeAttribute("disabled");
 }
